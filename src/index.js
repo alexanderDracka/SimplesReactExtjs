acc5caec8342c4f4e883548cba8d369e1320a480
AlexanderDracka <alexander.dracka@profiq.cz>
2017-01-16 12:44:44 +0100

    react and extjs within one component

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ install({
     // Setting viewport: true adds css rules to make the html, body, and the root react element height: 100% to
     // allow the root component to expand to fill the full screen. You should omit this option when using
     // other stylesheets or component libraries to control the layout.
-    viewport: true
+    viewport: false
 });
 
 // launch the react app once Ext JS is ready
-Ext.onReady(() => ReactDOM.render(<MyComponent/>, document.getElementById('root')));
+Ext.onReady(() => ReactDOM.render(
+    <div>
+        <h2>ExtJS Component:</h2>
+        <MyComponent/>
+    </div>, document.getElementById('root')));
